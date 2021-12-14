7b1077e2ae357d2b0e5a5d86dd554338077ba051
hmrs-cr <hmrs-cr@users.noreply.github.com>
2021-12-14 16:16:41 -0600

    Updated build files

---
+++ mobile/src/bj40_81490/java/com/hmsoft/locationlogger/ui/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.hmsoft.locationlogger.ui;
+
+import android.app.Activity;
+import android.content.Context;
+
+public class LoginActivity extends Activity  {
+    public static void start(Context c) {
+
+    }
+}
+
