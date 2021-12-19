86814556dc7351e6faad1be9b71e49e6f923790b
hmrs-cr <hmrs-cr@users.noreply.github.com>
2021-12-19 15:09:38 -0600

    Small improvements for info command

--- mobile/src/main/java/com/hmsoft/locationlogger/data/commands/InfoCommand.java
+++ mobile/src/main/java/com/hmsoft/locationlogger/data/commands/InfoCommand.java
@@ -20,11 +20,6 @@ class InfoCommand extends Command {
     @Override
     public void execute(String[] params, CommandContext context) {
         String info = Utils.getGeneralInfo(context.androidContext);
-
-        if(context.source == Command.SOURCE_SMS) {
-            Utils.sendSms(context.fromId, info, null);
-        } else {
-            CoreService.updateLocation(context.androidContext, info);
-        }
+        sendReply(context, info);
     }
 }
